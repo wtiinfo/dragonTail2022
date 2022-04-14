2923e2e047b0396d66cc9fa3ce0fc4720262db9b
wtiinfo <wanbrwan@gmail.com>
2022-04-14 13:27:51 -0300

    Tile

---
+++ src/tilemap/Tile.java
@@ -0,0 +1,22 @@
+package tilemap;
+
+import java.awt.image.BufferedImage;
+
+public class Tile {
+
+    private BufferedImage image;
+    private int type;
+
+    public Tile(BufferedImage image, int type) {
+        this.image = image;
+        this.type = type;
+    }
+
+    public BufferedImage getImage() {
+        return image;
+    }
+
+    public int getType() {
+        return type;
+    }
+}
